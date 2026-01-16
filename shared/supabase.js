da8a53e769918fec8e2082cea37db8a979dc2b0e
charolastra83 <129147435+charolastra83@users.noreply.github.com>
2026-01-16 05:58:06 -0600

    Add vecinos assets and supabase client

--- shared/supabase.js
+++ shared/supabase.js
@@ -5,8 +5,4 @@ const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5Y2VzZ3FveWNramZudHp6eXZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxOTU1NjMsImV4cCI6MjA4Mzc3MTU2M30.YGAEer_7NrK8Dy1dEU3G0oRWbrBoQTTfZSLdagdzG-E";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-<<<<<<< HEAD
 export default supabase;
-=======
-export default supabase;
->>>>>>> 6a39cf2383a1d0d4905c489df7f1c36aeb42b6ba
